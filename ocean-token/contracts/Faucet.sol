6a22faef58acdb9c670c72d5bc4bca2e48154440
Jonathan Spruance <jspruance@inflightcorp.com>
2022-09-17 00:08:07 -0400

    Adding formatting

--- ocean-token/contracts/Faucet.sol
+++ ocean-token/contracts/Faucet.sol
@@ -4,9 +4,11 @@
 pragma solidity ^0.8.17;
 
 interface IERC20 {
-  function transfer(address to, uint256 amount) external returns (bool);
-  function balanceOf(address account) external view returns (uint256);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
 contract Faucet {
@@ -27,39 +29,50 @@ contract Faucet {
     }
 
     function requestTokens() public {
-      require(msg.sender != address(0), "Request must not originate from a zero account");
-      require(token.balanceOf(address(this)) >= withdrawalAmount, "Insufficient balance in faucet for withdrawal request");
-      require(block.timestamp >= nextAccessTime[msg.sender], "Insufficient time elapsed since last withdrawal - try again later.");
-      
-      nextAccessTime[msg.sender] = block.timestamp + lockTime;
+        require(
+            msg.sender != address(0),
+            "Request must not originate from a zero account"
+        );
+        require(
+            token.balanceOf(address(this)) >= withdrawalAmount,
+            "Insufficient balance in faucet for withdrawal request"
+        );
+        require(
+            block.timestamp >= nextAccessTime[msg.sender],
+            "Insufficient time elapsed since last withdrawal - try again later."
+        );
 
-      token.transfer(msg.sender, withdrawalAmount);
+        nextAccessTime[msg.sender] = block.timestamp + lockTime;
+
+        token.transfer(msg.sender, withdrawalAmount);
     }
 
     receive() external payable {
-      emit Deposit(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
     }
 
     function getBalance() external view returns (uint256) {
-      return token.balanceOf(address(this));
+        return token.balanceOf(address(this));
     }
 
     function setWithdrawalAmount(uint256 amount) public onlyOwner {
-      withdrawalAmount = amount * (10**18);
+        withdrawalAmount = amount * (10**18);
     }
 
     function setLockTime(uint256 amount) public onlyOwner {
-      lockTime = amount * 1 minutes;
+        lockTime = amount * 1 minutes;
     }
 
     function withdraw() external onlyOwner {
-      emit Withdrawal(msg.sender, token.balanceOf(address(this)));
-      token.transfer(msg.sender, token.balanceOf(address(this)));
+        emit Withdrawal(msg.sender, token.balanceOf(address(this)));
+        token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
     modifier onlyOwner() {
-      require(msg.sender == owner, "Only the contract owner can call this function");
-      _;
+        require(
+            msg.sender == owner,
+            "Only the contract owner can call this function"
+        );
+        _;
     }
-
 }
