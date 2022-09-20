6d591bbedff6fa709bc1ef1e914c3420997c2bbf
Jonathan Spruance <jspruance@inflightcorp.com>
2022-09-20 16:08:14 -0400

    Adding eth folder

---
+++ faucet-ui/src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+
+function App() {
+  return (
+    <body>
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <h1 className="navbar-item is-size-4">Ocean Token (OCT)</h1>
+          </div>
+          <div id="navbarMenu" className="navbar-menu">
+            <div className="navbar-end is-align-items-center">
+              <button className="button is-white connect-wallet">
+                <span className="is-link has-text-weight-bold">
+                  Connect Wallet
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <section className="hero is-fullheight">
+        <div className="faucet-hero-body">
+          <div className="container has-text-centered main-content">
+            <h1 className="title is-1">Faucet</h1>
+            <p>Fast and reliable. 10 OCT/day.</p>
+            <div className="box address-box">
+              <div className="columns">
+                <div className="column is-four-fifths">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    placeholder="Enter your wallet address (0x...)"
+                  />
+                </div>
+                <div className="column">
+                  <button className="button is-link is-medium">
+                    GET TOKENS
+                  </button>
+                </div>
+              </div>
+              <article className="panel is-grey-darker">
+                <p className="panel-heading">Transaction Data</p>
+                <div className="panel-block">
+                  <p>transaction data</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    </body>
+  );
+}
+
+export default App;
