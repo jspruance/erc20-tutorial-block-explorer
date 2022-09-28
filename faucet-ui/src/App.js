fcde2cf35b016164704fe5ef144cdaef13b3f482
Jonathan Spruance <jspruance@inflightcorp.com>
2022-09-28 04:21:20 -0400

    Adding completed Faucet dApp code

--- faucet-ui/src/App.js
+++ faucet-ui/src/App.js
@@ -1,8 +1,100 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ethers } from "ethers";
+import faucetContract from "./ethereum/faucet";
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState("");
+  const [signer, setSigner] = useState();
+  const [fcContract, setFcContract] = useState();
+  const [withdrawError, setWithdrawError] = useState("");
+  const [withdrawSuccess, setWithdrawSuccess] = useState("");
+  const [transactionData, setTransactionData] = useState("");
+
+  useEffect(() => {
+    getCurrentWalletConnected();
+    addWalletListener();
+  }, [walletAddress]);
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        /* get provider */
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        /* get accounts */
+        const accounts = await provider.send("eth_requestAccounts", []);
+        /* get signer */
+        setSigner(provider.getSigner());
+        /* local contract instance */
+        setFcContract(faucetContract(provider));
+
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        /* get provider */
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        /* get accounts */
+        const accounts = await provider.send("eth_accounts", []);
+        if (accounts.length > 0) {
+          /* get signer */
+          setSigner(provider.getSigner());
+          /* local contract instance */
+          setFcContract(faucetContract(provider));
+          setWalletAddress(accounts[0]);
+          console.log(accounts[0]);
+        } else {
+          console.log("Connect to MetaMask using the Connect button");
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      });
+    } else {
+      /* MetaMask is not installed */
+      setWalletAddress("");
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const getOCTHandler = async () => {
+    setWithdrawError("");
+    setWithdrawSuccess("");
+    try {
+      const fcContractWithSigner = fcContract.connect(signer);
+      const resp = await fcContractWithSigner.requestTokens();
+      console.log(resp);
+      setWithdrawSuccess("Operation succeeded - enjoy your tokens!");
+      setTransactionData(resp.hash);
+    } catch (err) {
+      console.error(err.message);
+      setWithdrawError(err.message);
+    }
+  };
+
   return (
-    <body>
+    <div>
       <nav className="navbar">
         <div className="container">
           <div className="navbar-brand">
@@ -10,9 +102,17 @@ function App() {
           </div>
           <div id="navbarMenu" className="navbar-menu">
             <div className="navbar-end is-align-items-center">
-              <button className="button is-white connect-wallet">
+              <button
+                className="button is-white connect-wallet"
+                onClick={connectWallet}
+              >
                 <span className="is-link has-text-weight-bold">
-                  Connect Wallet
+                  {walletAddress && walletAddress.length > 0
+                    ? `Connected: ${walletAddress.substring(
+                        0,
+                        6
+                      )}...${walletAddress.substring(38)}`
+                    : "Connect Wallet"}
                 </span>
               </button>
             </div>
@@ -23,7 +123,15 @@ function App() {
         <div className="faucet-hero-body">
           <div className="container has-text-centered main-content">
             <h1 className="title is-1">Faucet</h1>
-            <p>Fast and reliable. 10 OCT/day.</p>
+            <p>Fast and reliable. 50 OCT/day.</p>
+            <div className="mt-5">
+              {withdrawError && (
+                <div className="withdraw-error">{withdrawError}</div>
+              )}
+              {withdrawSuccess && (
+                <div className="withdraw-success">{withdrawSuccess}</div>
+              )}{" "}
+            </div>
             <div className="box address-box">
               <div className="columns">
                 <div className="column is-four-fifths">
@@ -31,10 +139,15 @@ function App() {
                     className="input is-medium"
                     type="text"
                     placeholder="Enter your wallet address (0x...)"
+                    defaultValue={walletAddress}
                   />
                 </div>
                 <div className="column">
-                  <button className="button is-link is-medium">
+                  <button
+                    className="button is-link is-medium"
+                    onClick={getOCTHandler}
+                    disabled={walletAddress ? false : true}
+                  >
                     GET TOKENS
                   </button>
                 </div>
@@ -42,14 +155,18 @@ function App() {
               <article className="panel is-grey-darker">
                 <p className="panel-heading">Transaction Data</p>
                 <div className="panel-block">
-                  <p>transaction data</p>
+                  <p>
+                    {transactionData
+                      ? `Transaction hash: ${transactionData}`
+                      : "--"}
+                  </p>
                 </div>
               </article>
             </div>
           </div>
         </div>
       </section>
-    </body>
+    </div>
   );
 }
 
---
+++ faucet-ui/src/ethereum/faucet.js
@@ -0,0 +1,159 @@
+import { ethers } from "ethers";
+
+const faucetAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "tokenAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "payable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "Deposit",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "Withdrawal",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "getBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "lockTime",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "requestTokens",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "setLockTime",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "setWithdrawalAmount",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "token",
+    outputs: [
+      {
+        internalType: "contract IERC20",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdrawalAmount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
+
+const faucetContract = (provider) => {
+  return new ethers.Contract(
+    "0xE16738Fb636c83b198A71368dd0D580FBc3B993B",
+    faucetAbi,
+    provider
+  );
+};
+
+export default faucetContract;
