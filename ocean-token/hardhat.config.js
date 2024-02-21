c63d62d48771335da5744ec6df6c8e3a1664361c
MadMoneyLab <119470128+MadMoneyLab@users.noreply.github.com>
2024-02-21 04:03:43 -0500

    Update hardhat.config.js

--- ocean-token/hardhat.config.js
+++ ocean-token/hardhat.config.js
@@ -3,14 +3,10 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.20",
   networks: {
-    rinkeby: {
-      url: process.env.INFURA_RINKEBY_ENDPOINT,
-      accounts: [process.env.PRIVATE_KEY]
-    },
-    goerli: {
-      url: process.env.INFURA_GOERLI_ENDPOINT,
+    sepolia: {
+      url: process.env.INFURA_SEPOLIA_ENDPOINT,
       accounts: [process.env.PRIVATE_KEY]
     }
   }
